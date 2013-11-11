0a0321a0d992e6fe81f853e2e018d4f58667d044
Niko Schwarz <niko.schwarz@gmail.com>
2013-11-11 09:57:41 +0100

    Modify DFATableTest to match optimization crap.

--- tests/ch/unibe/scg/regex/DFATableTest.java
+++ tests/ch/unibe/scg/regex/DFATableTest.java
@@ -1,7 +1,6 @@
 package ch.unibe.scg.regex;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Before;
@@ -16,45 +15,46 @@ public final class DFATableTest {
   static final int s3 = 2;
   static final int s4 = 3;
   TDFATransitionTable table;
+  NextState n;
 
   @Before
   public void setUp() {
     table =
         new TDFATransitionTable(new char[] {'c', 'l'}, new char[] {'k', 'm'}, new int[] {s1, s2},
             new int[] {s3, s4}, new Instruction[][] {new Instruction[] {}, new Instruction[] {}});
+    n = new NextState();
   }
 
   @Test
   public void testTable() {
-    final NextState pr = table.newStateAndInstructions(200, 'd');
-    assertThat(pr, is(nullValue()));
+    table.newStateAndInstructions(200, 'd', n);
+    assertThat(n.found, is(Boolean.FALSE));
   }
 
   @Test
   public void testTable2() {
-    final NextState pr = table.newStateAndInstructions(s1, 'c');
-    assertThat(pr.nextState, is(s3));
-
+    table.newStateAndInstructions(s1, 'c', n);
+    assertThat(n.found, is(Boolean.TRUE));
+    assertThat(n.nextState, is(s3));
   }
 
   @Test
   public void testTable3() {
-    final NextState pr = table.newStateAndInstructions(s1, 'k');
-    assertThat(pr.nextState, is(s3));
-
+    table.newStateAndInstructions(s1, 'k', n);
+    assertThat(n.found, is(Boolean.TRUE));
+    assertThat(n.nextState, is(s3));
   }
 
   @Test
   public void testTable4() {
-    final NextState pr = table.newStateAndInstructions(s1, 'l');
-    assertThat(pr, is(nullValue()));
-
+    table.newStateAndInstructions(s1, 'l', n);
+    assertThat(n.found, is(Boolean.FALSE));
   }
 
   @Test
   public void testTable5() {
-    final NextState pr = table.newStateAndInstructions(s2, 'l');
-    assertThat(pr.nextState, is(s4));
+    table.newStateAndInstructions(s2, 'l', n);
+    assertThat(n.found, is(Boolean.TRUE));
+    assertThat(n.nextState, is(s4));
   }
-
 }
