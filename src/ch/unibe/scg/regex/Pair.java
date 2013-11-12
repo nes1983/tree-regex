bf01dbecde367ca172fdef8183c3d5d9541a2f75
Niko Schwarz <niko.schwarz@gmail.com>
2013-11-12 11:49:10 +0100

    Further simplify pair.

--- src/ch/unibe/scg/regex/Pair.java
+++ src/ch/unibe/scg/regex/Pair.java
@@ -1,22 +1,16 @@
 package ch.unibe.scg.regex;
 
+import java.util.Objects;
+
 /**
  * A generic class representing a {@link Pair} of two kind of {@link Object}
  *
  * @author Fabien Dubosson
- * @param <A> The first {@link Object} {@link Class} type
- * @param <B> The second {@link Object} {@link Class} type
  */
 class Pair<A extends Comparable<A>, B extends Comparable<B>> implements Comparable<Pair<A, B>> {
   final A first;
   final B second;
 
-  /**
-   * Constructor taking the two {@link Object} as parameters
-   *
-   * @param first The first {@link Object} of the {@link Pair}
-   * @param second The second {@link Object} of the {@link Pair}
-   */
   Pair(final A first, final B second) {
     this.first = first;
     this.second = second;
@@ -24,11 +18,11 @@ class Pair<A extends Comparable<A>, B extends Comparable<B>> implements Comparab
 
   @Override
   public int compareTo(final Pair<A, B> o) {
-    final int result = (this.first).compareTo(o.first);
+    final int result = first.compareTo(o.first);
     if (result != 0) {
       return result;
     }
-    return this.second.compareTo(o.second);
+    return second.compareTo(o.second);
   }
 
   @Override
@@ -62,11 +56,7 @@ class Pair<A extends Comparable<A>, B extends Comparable<B>> implements Comparab
 
   @Override
   public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((first == null) ? 0 : first.hashCode());
-    result = prime * result + ((second == null) ? 0 : second.hashCode());
-    return result;
+    return Objects.hash(first, second);
   }
 
   @Override
